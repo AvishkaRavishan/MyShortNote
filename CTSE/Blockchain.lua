d51a75f214774735194e6cc60891cf4a79e578b8
Ravishan S.A.A <avishk678@icloud.com>
2023-06-02 20:52:57 +0530

    ctse_blockchain

---
+++ CTSE/Blockchain.lua
@@ -0,0 +1,51 @@
++---------------------+
+|                     |
+|       Block 1       |
+|                     |
++---------------------+
+          |
+          V
++---------------------+
+|                     |
+|       Block 2       |
+|                     |
++---------------------+
+          |
+          V
++---------------------+
+|                     |
+|       Block 3       |
+|                     |
++---------------------+
+          |
+          V
++---------------------+
+|                     |
+|       Block 4       |
+|                     |
++---------------------+
+          |
+          V
+       ...
+          |
+          V
++---------------------+
+|                     |
+|       Block N       |
+|                     |
++---------------------+
+
+
+##
+Key Elements:
+
+Blocks: Each block represents a collection of data.
+Chain: Blocks are linked together in a specific order, forming a chain.
+Hash: Each block has a unique identifier called a hash, created by applying a cryptographic function to the block's data.
+Links: Each block contains a reference to the hash of the previous block, creating a strong connection between blocks.
+Nodes: Multiple copies of the chain exist in a decentralized network, represented by individual nodes.
+Consensus: Nodes agree on the validity of transactions and the order in which they are added to the chain through a consensus mechanism.
+Security: Blocks are secured through cryptographic hashing, making it difficult to tamper with the data.
+Transparency: The blockchain is visible to all participants, providing transparency and immutability.
+Adding New Blocks: Validated transactions are added as new blocks to the chain, extending its length.
+##
