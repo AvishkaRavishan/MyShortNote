be7afd5516e62089c3fb141e953dca05940ba5ba
Ravishan S.A.A <avishk678@icloud.com>
2023-06-02 20:55:25 +0530

    bchain-ctse

---
+++ MADD/L_1.swift
@@ -0,0 +1,72 @@
+print("Hello world")
+
+var no1 = 80
+print(no1)
+var no2 = 10.2
+print(no2)
+var name = "Ajith"
+print(name)
+var result = true
+print(result)
+
+var no3 = 4.5
+no3 = Double(no1) + no2
+print(no3)
+
+let conversionRate = 1.6
+print(conversionRate)
+
+let pi : Double
+pi = 3.14
+print(pi)
+//pi = 3.141
+print(pi)
+
+var marks1 : Int // type annotation
+var marks2 : Int
+// Int, String, Double, Bool
+
+marks1 = 78
+marks2 = 90
+print (marks1)
+
+// String interpolation
+print("Student \(name) has got \(marks1) and \(marks2) for the exam")
+
+// 1) Write a program where you have
+// the total CA and then final exam mark of a student
+// following SE4020.  CAlculate and print his/her final
+// marks.
+
+// Use a constant to represent the CA% which is 70%
+
+let CAPer : Double = 0.7
+var ca = 100
+var finalexam = 0
+
+var final = Double(ca) * CAPer + Double(finalexam) * (1-CAPer)
+print("Final Marks is \(final)")
+
+// java
+Person p1 = (dsfdsfsd)
+Person p2
+
+p2 = p1
+p2 = "dfdfs"
+
+UIKit
+classes
+
+Structures and Class
+
+SwingUI - classes are not used
+Structures are used
+
+Structure variables are passed by value
+PersonStruct p3 = (dfds)
+PersonStruct p4
+
+p4 = p3
+
+
+
